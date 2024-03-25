f0b1cba7620520d5db64e58b2e2d8733129cdddb
louis <louis.lamonte@moringaschool.com>
2024-03-25 20:55:10 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee = {
+    name: "Janet",
+    streetAddress:"Uchumi 27"
+    
+}
+
+function updateEmployeeWithKeyAndValue(employee,key ,value){
+    let updatedEmployee = (...employee)
+    updatedEmployee [key] = value
+    return updatedEmployee
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+employee[key] = value
+return employee
+
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    let updatedEmployee = {...employee}
+    delete updatedEmployee[key]
+    return updatedEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
